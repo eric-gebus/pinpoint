78bcbc650f39bb5d30cf0915ed019b74d6463db2
tylaur <tylerjmunz@gmail.com>
2025-05-06 11:01:17 -0400

    up-to-date list component

--- client/src/component/List.tsx
+++ client/src/component/List.tsx
@@ -1,11 +1,17 @@
+import Element from "./Element";
+
 interface ListProps {
   eventList: Event[];
 }
-function List({ eventList }: ListProps) {
 
+function List({ eventList }: ListProps) {
   return (
-    <div>
-      <h1>List Component</h1>
+    <div className="flex m-2 flex-col gap-2 max-w-md">
+      { 
+        eventList.map((event, index) => (
+          <Element key={index} event={event}/>
+        ))
+      }
     </div>
   );
 }
