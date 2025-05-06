47c6ba7c5266dd023370104909fdbd6ac65722be
eric-gebus <ericgebus@gmail.com>
2025-05-06 08:23:18 +0100

    Removed unnecessary code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -65,50 +65,7 @@ function App() {
           <Route path="/map" element={<Map position={position} />} />
           <Route path="/list" element={<List />} />
         </Routes>
-        {/* search-nav-container */}
-        <div className="flex place-content-between p-2">
-          {/* search-input */}
-          <label className="flex items-center">
-            <svg
-              className="h-[1em] mr-2 opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-              </g>
-            </svg>
-            <input type="search" required placeholder="Search" />
-          </label>
-          {/* button-container */}
-          <div className="bg-gradient-to-b from-stone-300/40 to-transparent p-[4px] rounded-[16px]">
-            <button
-              className="group p-[4px] rounded-[12px] bg-gradient-to-b from-white to-stone-200/40 shadow-[0_1px_3px_rgba(0,0,0,0.5)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995]"
-              onClick={getLocation}
-            >
-              <div className="bg-gradient-to-b from-stone-200/40 to-white/80 rounded-[8px] px-2 py-2">
-                <div className="flex gap-2 items-center">
-                  <span className="font-semibold">Get my position</span>
-                </div>
-              </div>
-            </button>
-          </div>
-        </div>
-        {/* map-container */}
-        <div className="basis-full">
-          <div className="m-3 overflow-hidden rounded-xl shadow-lg/50">
-            <Map position={position} />
-          </div>
-        </div>
       </Router>
-      <Weather position={position}/>
       {/* search-nav-container */}
       <div className="flex place-content-between p-2 ">
         {/* search-input */}
