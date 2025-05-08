02df0f2f1292bb6cd3b9ccf91412b05e1ebb9da7
liya-mary <maryliya007@gmail.com>
2025-05-08 11:34:04 +0200

    Add redirect page from pin details and resolve address bug

--- client/src/component/Pin.tsx
+++ client/src/component/Pin.tsx
@@ -32,10 +32,10 @@ function Pin({position,eventList}:PinProps) {
                 <Popup>
                   <>
                     <img src={event.images[0].url} alt="" />
-                    <h1> <b>{event.name}</b></h1>
-                    <h5>{event._embedded.venues[0].address.line1}</h5>
-                    <h5>{event.distance} km away</h5>
-                    <a href={event.url}>Book now</a>
+                    <h1 className="m-1"> <b>{event.name}</b></h1>
+                    <h5 className="m-1">{event._embedded.venues[0].address? event._embedded.venues[0].address.line1:""}</h5>
+                    <h5 className="m-1">{event.distance} km away</h5>
+                    <h4 className="text-l font-light m-1 text-blue-600 cursor-pointer hover:underline" onClick={() => window.open(event.url)}>Book now</h4>
                   </>
                 </Popup>
               </Marker>
@@ -45,7 +45,7 @@ function Pin({position,eventList}:PinProps) {
         {      
            eventList.length>0&&<Marker position={position} icon={myIcon}>
                 <Popup>
-                    Your location <br /> 
+                    You are here! <br /> 
                 </Popup>
             </Marker>
         }
