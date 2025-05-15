6025e64939b3f7e196707d7896ed6d614b998a88
eric-gebus <ericgebus@gmail.com>
2025-05-15 11:04:37 +0100

    Add Weather tests

--- client/src/component/Weather.test.tsx
+++ client/src/component/Weather.test.tsx
@@ -1,12 +1,9 @@
 import { render, screen} from "@testing-library/react";
-import { describe, beforeAll, expect, it, test, expectTypeOf } from "vitest";
+import { describe, beforeAll, expect, it} from "vitest";
 import "@testing-library/jest-dom/vitest";
 import Weather from "./Weather";
-import { fetchWeatherApi } from "openmeteo";
-import { WeatherApiResponse } from "@openmeteo/sdk/weather-api-response";
 
-const BEFORE_ALL_TIMEOUT = 30000;
-const position = [51.5074, -0.1278]
+
 
 describe ("Weather", () => {
   beforeAll(() => {
@@ -24,35 +21,4 @@ describe ("Weather", () => {
 
 })
 
-describe('Request Weather API', () => {
-
-  beforeAll(async () => {
-    const params = {
-      "latitude": position[0],
-      "longitude": position[1],
-      "hourly": ["temperature_2m", "weather_code", "precipitation_probability"],
-      "timezone": "auto",
-    };
-
-    const url = "https://api.open-meteo.com/v1/forecast";
-
-    const response = await fetchWeatherApi(url, params);
-
-  }, BEFORE_ALL_TIMEOUT);
-
-
-
-  // test('Should have response status 200', () => {
-  //   expect(response.status).toBe(200);
-
-  // test('Should have content-type', () => {
-  //   expect(response.headers.get('Content-Type')).toBe('application/json');
-  // })
-
-  // test('Should hgave array in the body', () => {
-  //   expectTypeOf(body).toBeArray();
-  // })
-// })
-
-})
 
